6b3561fd3fed18f4df9fd9dfb7de7076b7699184
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-02-14 14:11:39 -0300

    change color text only for test

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const allButtons = document.querySelectorAll(".drum");
 
 allButtons.forEach(buttonElement => {
   buttonElement.addEventListener("click",function () {
-    var audio = new Audio("./sounds/crash.mp3");
-    audio.play();  
+    console.log(this.textContent);
+    this.style.color="orange";    
   })
 });
+
+
+// var audio = new Audio("./sounds/crash.mp3");
+//     audio.play();  
